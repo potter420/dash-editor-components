82afcee8daf4a21abeef65fe570a450b8830aad6
Tommy Tran <thanhmt0612@gmail.com>
2020-03-13 10:06:29 +0700

    Update PythonEditor.react.js

--- src/lib/components/PythonEditor.react.js
+++ src/lib/components/PythonEditor.react.js
@@ -4,7 +4,10 @@ import classnames from 'classnames';
 import Editor from 'react-simple-code-editor';
 import { highlight, languages } from 'prismjs/components/prism-core';
 import 'prismjs/components/prism-python';
-import 'primsjs/components/prism-javascript';
+import 'prismjs/components/prism-markup';
+import 'prismjs/components/prism-markup-templating';
+import 'prismjs/components/prism-clike';
+import 'prismjs/components/prism-javascript';
 
 /**
  * PythonEditor is a Python code editor comopnent.
